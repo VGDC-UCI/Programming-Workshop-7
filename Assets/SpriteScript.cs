0e78d54567c1254c36c5c197f0a6b8b50671a112
turtlewit <turtlewit@gmail.com>
2018-11-28 15:13:18 -0800

    initial commit

---
+++ Assets/SpriteScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScript : MonoBehaviour {
+
+	Animator myController;
+	public AudioSource audio1;
+	public AudioSource audio2;
+
+	// Use this for initialization
+	void Start () {
+		myController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			myController.SetTrigger("doCircle");
+		}
+		if (Input.GetAxisRaw("Horizontal") > 0 && audio1.isPlaying == false) {
+			audio1.Play();
+		}
+		if (Input.GetAxisRaw("Horizontal") < 0 && audio2.isPlaying == false) {
+			audio2.Play();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Programming-Workshop-7
+
+Hello, VGDC Programmers! Before the workshop, please clone this repo:  
+* `git clone https://github.com/VGDC-UCI/Programming-Workshop-7.git` 
+* Or use Github Desktop / another git frontend
+
+Or download a zip file  
+* Clone or Download -> Download ZIP
